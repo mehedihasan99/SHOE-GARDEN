44a6f3761b768cf9a2a7a304505dcadea7bbbaea
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-26 18:21:39 +0600

    difference between props and state

--- src/components/TheoryJs/Theory.js
+++ src/components/TheoryJs/Theory.js
@@ -40,6 +40,12 @@ const Theory = () => {
             </ul>
           </div>
       </div>
+      <div>
+          <h4>HOW useState WORKS </h4>
+          <p>useState is a Hook that allows you to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.
+          useState enables you to add state to function components. Calling React.useState inside a function component generates a single piece of state associated with that component
+          </p>
+      </div>
     </div>
   );
 };
